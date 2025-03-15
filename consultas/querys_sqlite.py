242286ba30c719a15bd12c7e76ad7cb94256bd55
devfzn <dev_fzn@proton.me>
2025-03-15 00:47:20 -0300

    temp fix 404 map overflow

--- consultas/querys_sqlite.py
+++ consultas/querys_sqlite.py
@@ -13,13 +13,13 @@ c = conn.cursor()
 # GeoLoc
 def get_geoloc(codigo):
     if codigo == 200:
-        consulta = """
-            SELECT DISTINCT geoloc FROM registro WHERE ip in ( SELECT `ip` from visita WHERE cod_html=='200');
-                    """
+        consulta = """SELECT DISTINCT geoloc FROM registro WHERE ip in
+                      (SELECT `ip` from visita WHERE cod_html=='200');"""
     else:
-        consulta = """
-            SELECT DISTINCT geoloc FROM registro WHERE ip in ( SELECT `ip` from visita WHERE cod_html!='200');
-                    """
+        consulta = """SELECT DISTINCT geoloc FROM registro WHERE ip in
+                      (SELECT `ip` from visita WHERE cod_html!='200'
+                      AND cod_html!='444' AND cod_html!='0'
+                      AND cod_html!='404');"""
     c.execute(consulta)
     resp = c.fetchall()
     return resp
@@ -28,16 +28,15 @@ def get_geoloc(codigo):
 # Detalle Visitas por pais
 def vistas_pais_detalle(pais, codigo=''):
     if codigo == '':
-        consulta = f"""
-        SELECT DATE(fecha, 'unixepoch') AS fecha_local, ip, metodo, cod_html, consulta
-         FROM visita WHERE ip IN (SELECT `ip` FROM `registro` WHERE `pais` = '{pais}');
-        """
+        consulta = f"""SELECT DATE(fecha, 'unixepoch') AS
+                       fecha_local, ip, metodo, cod_html, consulta
+                       FROM visita WHERE ip IN (SELECT `ip` FROM `registro`
+                       WHERE `pais` = '{pais}');"""
     else:
-        consulta = f"""
-        SELECT DATE(fecha, 'unixepoch') AS fecha_local, ip, metodo, cod_html, consulta
-        FROM visita WHERE ip IN (SELECT `ip` FROM `registro` WHERE `pais` = '{pais}')
-        and cod_html = '{codigo}';
-        """
+        consulta = f"""SELECT DATE(fecha, 'unixepoch') AS
+                       fecha_local, ip, metodo, cod_html, consulta
+                       FROM visita WHERE ip IN (SELECT `ip` FROM `registro`
+                       WHERE `pais` = '{pais}') and cod_html = '{codigo}';"""
     c.execute(consulta)
     resp = c.fetchall()
     return resp
@@ -61,8 +60,8 @@ def pt_visita_pais_detalle(pais, codigo=''):
     tbl_v.add_column("Respuesta", justify="left", style="bright_blue")
     tbl_v.add_column("Consulta", justify="left", style="yellow")
     for item in respuesta:
-        tbl_v.add_row(str(item[0]), str(item[1]), str(item[2]), str(item[3]), str(item[4]))
-
+        tbl_v.add_row(str(item[0]), str(item[1]), str(item[2]),
+                      str(item[3]), str(item[4]))
     console.print(tbl_v)
 
 
@@ -75,12 +74,11 @@ def unix_to_local_date():
     return c.fetchall()
 
 
-# Select geoloc by cod html -- SELECT all from registro where ip=(SELECT ip from visita where cod_html=200);
+# Select geoloc by cod html
+# SELECT all from registro where ip=(SELECT ip from visita where cod_html=200);
 def select_cod(codigo):
-    consulta = f"""
-    SELECT geoloc FROM registro WHERE ip IN
-       (SELECT ip FROM visita WHERE cod_html = {codigo});
-    """
+    consulta = f"""SELECT geoloc FROM registro WHERE ip IN
+                   (SELECT ip FROM visita WHERE cod_html = {codigo});"""
     c.execute(consulta)
     resp = c.fetchall()
     return resp
@@ -88,10 +86,10 @@ def select_cod(codigo):
 
 # Select fecha mayor que (ej. 1661226920)
 def sel_pais_desde(pais, unix_e):
-    consulta = f"""
-    SELECT DATE(fecha, 'unixepoch') AS fecha_local, ip, metodo, cod_html, consulta
-    FROM visita WHERE ip IN (SELECT `ip` FROM `registro` WHERE `pais` = '{pais}') and fecha > {unix_e};
-    """
+    consulta = f"""SELECT DATE(fecha, 'unixepoch') AS
+                   fecha_local, ip, metodo, cod_html, consulta
+                   FROM visita WHERE ip IN (SELECT `ip` FROM
+                   `registro` WHERE `pais` = '{pais}') and fecha > {unix_e};"""
     c.execute(consulta)
     resp = c.fetchall()
     return resp
@@ -113,22 +111,16 @@ def pt_sel_pais_fecha(pais, fecha_ux, fecha_loc):
     tbl_v.add_column("Respuesta", justify="left", style="bright_blue")
     tbl_v.add_column("Consulta", justify="left", style="yellow")
     for item in respuesta:
-        tbl_v.add_row(str(item[0]), str(item[1]), str(item[2]), str(item[3]), str(item[4]))
-
+        tbl_v.add_row(str(item[0]), str(item[1]),
+                      str(item[2]), str(item[3]), str(item[4]))
     console.print(tbl_v)
 
 
 # Top 50 paises
 def top_paises(top):
-    consulta = f"""
-    SELECT registro.pais,
-        count(registro.pais) as Visitas
-        FROM visita, registro
-            WHERE visita.ip=registro.ip
-        GROUP BY pais
-        ORDER BY count(*) DESC
-        LIMIT {top};
-    """
+    consulta = f"""SELECT registro.pais, count(registro.pais) as Visitas
+                   FROM visita, registro WHERE visita.ip=registro.ip
+                   GROUP BY pais ORDER BY count(*) DESC LIMIT {top};"""
     c.execute(consulta)
     resp = c.fetchall()
     return resp
@@ -136,12 +128,11 @@ def top_paises(top):
 
 def pt_top_paises(top):
     respuesta = top_paises(top)
-    tbl_v = Table(
-            title=f"[bold][blue]Vistas Top {top}[/blue][/bold]",
-            box=box.ROUNDED,
-            show_lines=False,
-            row_styles=["dim", ""],
-            border_style="dark_magenta")
+    tbl_v = Table(title=f"[bold][blue]Vistas Top {top}[/blue][/bold]",
+                  box=box.ROUNDED,
+                  show_lines=False,
+                  row_styles=["dim", ""],
+                  border_style="dark_magenta")
     tbl_v.add_column("País", justify="center", style="bright_yellow")
     tbl_v.add_column("Visitas", justify="left", style="bright_cyan")
     for item in respuesta:
@@ -152,14 +143,10 @@ def pt_top_paises(top):
 
 # respuesta HTML para pais=?
 def cod_html_pais(pais):
-    consulta = f"""
-    SELECT cod_html,
-            count(cod_html) AS Ocurrencias
-            FROM visita
-            WHERE ip IN (SELECT `ip` FROM `registro` WHERE `pais` = '{pais}')
-            GROUP BY cod_html
-            ORDER BY count(*) DESC;
-    """
+    consulta = f"""SELECT cod_html, count(cod_html) AS Ocurrencias
+                   FROM visita WHERE ip IN (SELECT `ip` FROM `registro`
+                   WHERE `pais` = '{pais}') GROUP BY cod_html
+                   ORDER BY count(*) DESC;"""
     c.execute(consulta)
     resp = c.fetchall()
     return resp
@@ -168,11 +155,11 @@ def cod_html_pais(pais):
 def pt_html_cod_pais(pais):
     respuesta = cod_html_pais(pais)
     tbl_v = Table(
-            title=f"[bold][blue]Códigos html: [/blue][green]{pais}[/bold][/green]",
-            box=box.ROUNDED,
-            show_lines=False,
-            row_styles=["dim", ""],
-            border_style="dark_magenta")
+        title=f"[bold][blue]Códigos html: [/blue][green]{pais}[/bold][/green]",
+        box=box.ROUNDED,
+        show_lines=False,
+        row_styles=["dim", ""],
+        border_style="dark_magenta")
     tbl_v.add_column("Código", justify="center", style="bright_yellow")
     tbl_v.add_column("Conteo", justify="left", style="bright_cyan")
     for item in respuesta:
